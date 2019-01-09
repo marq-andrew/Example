635c891a5090e3b581740784211456c1fd28d8e8
Mark Andrew <mchugh@ozemail.com.au>
2019-01-10 08:27:40 +1100

    Initial Commit

---
+++ Example/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Example
+//
+//  Created by Mark McHugh on 10/1/19.
+//  Copyright © 2019 Mark McHugh. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Example/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Mark McHugh on 10/1/19.
+//  Copyright © 2019 Mark McHugh. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
